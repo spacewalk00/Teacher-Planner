d5646cc4d680c282e3c1884c0bc1f0586f9ce4f5
spacewalk00 <ju001217@gmail.com>
2025-11-26 00:43:34 +0900

    feat : Bottom Navigation 적용 및 프로젝트 색상 정의

---
+++ app/screens/Setting.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function Setting() {
+    return(
+        <View className="flex-1 justify-center items-center bg-surface">
+            <Text className="text-3xl font-bold text-primary">Teacher Planner</Text>
+            <Text className="text-neutral mt-2">설정</Text>
+        </View>
+    );
+}
